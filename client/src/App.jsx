2491f58ee342747280a588e0b2a12837662a9f68
tristanborgess <tristan@borgessolari.com>
2023-10-04 19:56:13 -0500

    restructured files and added data directly to the front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,21 @@
-import { useState } from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
-import Navbar from './Navbar';
-import Wallets from './Wallets';
-import Footer from './Footer';
-import { UserProvider } from './UserContext';
-import Signin from './Signin';
-import Profile from './Profile';
-import Signup from './Signup';
+import Navbar from './components/Navbar';
+import Wallets from './components/Wallets';
+import Footer from './components/Footer';
 
 
 function App() {
 
   return (
-    <UserProvider>
       <BrowserRouter>
       <GlobalStyles />
         <Navbar />
         <Routes>
           <Route path='/' element={<Wallets />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<Signup />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-    </UserProvider>
   );
 }
 
